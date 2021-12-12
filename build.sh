003a338b4c4bc42d120ab2edd431bb559bf8d33e
mrbeepot <36437651+mrbeepot@users.noreply.github.com>
2021-12-12 10:32:45 +0530

    Created a bash script for creating a express project

---
+++ build.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Check if jq is installed
+if ! command -v jq &> /dev/null
+then
+    echo "jq is not installed."
+    echo "To install it, refer to: https://stedolan.github.io/jq/download/"
+    exit 1
+fi
+
+# Read the app name
+appName=$1
+
+# Check if typescript is required
+if [ "$2" = "ts" ]
+then
+    isTypescript=true
+else
+    isTypescript=false
+fi
+
+# Create directory if it does not exist
+mkdir -p "$appName"
+
+# change directory to that folder
+cd "$appName"
+
+# Create package.json file with default settings.
+yarn init --yes
+
+# Install express.js
+yarn add express
+
+# Install nodemon
+yarn add -D nodemon
+
+# Install typescript related dependencies if required.
+if [ $isTypescript = true ]
+then
+    yarn add -D @types/node @types/express nodemon typescript
+fi
+
+# Create a .editorconfig
+touch .editorconfig
+cat >.editorconfig <<EOL
+root = true
+
+[*]
+trim_trailing_whitespace = true
+insert_final_newline = true
+end_of_line = lf
+
+[*.yml]
+indent_style = space
+indent_size = 2
+tab_width = 2# This is actually a node library. Source code: https://github.com/benawad/tsconfig.json
+    npx tsconfig.json
+[*.{js,ts,json,html,css}]
+indent_style = tab
+indent_size = 4
+tab_width = 4
+
+EOL
+
+# Create a .env
+
+touch .env
+cat >.env <<EOL
+BROWSER=none
+EOL
+
+# Initialize a git repo
+git init
+
+# Create a tsconfig.json
+if [ $isTypescript = true ]
+then
+    # This is actually a node library. Source code: https://github.com/benawad/tsconfig.json
+    npx tsconfig.json
+fi
